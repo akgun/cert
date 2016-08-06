c8f28a746fab13b229742927fbb23645fc946747
Akgun Demirbas <akgun.demirbas@gmail.com>
2016-08-06 22:46:51 +0300

    Added removing outputs.

--- cert/entrypoint.sh
+++ cert/entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 cmd=$1
 
 case $cmd in
-    build) exec pyinstaller --onefile main.spec ;;
-    test) exec python3 -m unittest discover tests ;;
+    build)
+        rm -rf build/ dist/
+        exec pyinstaller --onefile main.spec ;;
+    test)
+        exec python3 -m unittest discover tests ;;
 esac
 
 exec "$@"
