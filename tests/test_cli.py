98f489b1e70f7d860608a705c42b924591cafc18
Akgun Demirbas <akgun.demirbas@gmail.com>
2016-09-04 12:56:01 +0300

    Changed test.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,7 +6,8 @@ from cert.cli import get_args
 class TestCli(unittest.TestCase):
 
     def test_get_args_install(self):
-        args = get_args(['https', '--domain', 'example.com', '--email', 'example@example.com', '--default-conf', '/etc/nginx/default.conf'])
+        args = get_args(['https', '--domain', 'example.com', '--email', 'example@example.com',
+                    '--default-conf', '/etc/nginx/default.conf', '--web-root', '/var/www/html'])
         self.assertEqual('example.com', args['domain'])
         self.assertEqual('example@example.com', args['email'])
         self.assertFalse(args['live'])
