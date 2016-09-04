a8a23e5d407dad077773798b6a1fb407ff3ff274
Akgun Demirbas <akgun.demirbas@gmail.com>
2016-09-04 15:49:43 +0300

    Changed test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 nose2 -v --with-coverage --coverage cert --coverage-report html --coverage-report xml
 pylint cert -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt
