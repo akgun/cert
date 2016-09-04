6d8ed2a49fdd3194fc8b973e0b202da895a206f3
Akgun Demirbas <akgun.demirbas@gmail.com>
2016-09-04 15:46:49 +0300

    Added pylint.

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 nose2 -v --with-coverage --coverage cert --coverage-report html --coverage-report xml
+pylint cert -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt
