56853e88bae50592a705e143425202e258a4e367
Akgun Demirbas <akgun.demirbas@gmail.com>
2016-09-04 16:00:43 +0300

    Disabled pylint error code.

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -ex
+set -x
 
 nose2 -v --with-coverage --coverage cert --coverage-report html --coverage-report xml
+
+set +e
 pylint cert -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt
+set -e
